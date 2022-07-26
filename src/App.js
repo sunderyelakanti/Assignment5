585013aad6577abf7574e7e531e3114b45afdc6b
sunderyelakanti <109147090+sunderyelakanti@users.noreply.github.com>
2022-07-26 18:50:47 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,32 +1,67 @@
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
 import product from './products.json'
-import CardGroup from 'react-bootstrap/CardGroup';
+import { useState } from 'react'
 function GridExample() {
+  const [searchTerm, setSearchTerm] = useState('')
+  const [checked, setChecked] = useState(false)
+ var handleFilter=(e)=>{
+    if(e.target.checked){
+      return e.value
+    } else if(e.target.unchecked === ""){
+
+    }
+  }
   return (
-    <Row xs={1} md={2} className="g-4">
-      {product.products.map((item, idx) => (
-      <Col>
-        <CardGroup>
-        <Card>
-          <Card.Img variant="top" src={item.images[0]} />
-          <Card.Body>
-            <Card.Title>{item.title}</Card.Title>
-            <Card.Text>{item.price} </Card.Text>
-            <Card.Text>{item.category} </Card.Text>
-            <Card.Text>{item.rating} </Card.Text>
-          </Card.Body>
-          {/* <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </Card.Footer> */}
-        </Card>
-        
-      </CardGroup>
-      </Col>
-       
-      ))}
-    </Row>
+
+    <div className='row'>
+      <input type="text" placeholder="search..." onChange={(event) => {setSearchTerm(event.target.value)}}/>
+      <>
+      <div>
+      <input type="checkbox" id="fragrances" name="interest" value="fragrances" onChange={handleFilter}/>
+      <label for="fragrances">fragrances</label>
+      </div>
+      <div>
+      <input type="checkbox"  id="laptops" name="interest" value="laptops"/>
+      <label for="laptops">laptops</label>
+      </div>
+      <div>
+      <input type="checkbox" id="smartphones" name="interest" value="smartphones"/>
+      <label for="smartphones">smartphones</label>
+      </div>
+      </>
+      <Container>
+        <Row>
+          {product.products.filter((item)=> {
+            if (searchTerm === "") {
+              return item
+            } 
+            else if(item.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+              return item
+            }
+            
+          }).map((item, idx) => (
+            <Col ls={6} style={{ display: "inline-grid", width: "35rem" }}>
+
+              <Card className="grid-container" idx={idx}>
+                <Card.Img variant="top" src={item.images[0]} />
+                <Card.Body>
+                  <Card.Title>{item.title}</Card.Title>
+                  <Card.Text>{item.price} </Card.Text>
+                  <Card.Text>{item.category} </Card.Text>
+                  <Card.Text>{item.rating} </Card.Text>
+                </Card.Body>
+
+              </Card>
+            </Col>
+
+          ))}
+        </Row>
+      </Container>
+
+    </div>
   );
 }
 
